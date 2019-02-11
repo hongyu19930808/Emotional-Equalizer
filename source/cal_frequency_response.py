dccf02555732ef8f9e04a6096508e066be3887fe
hongyu19930808 <hongyu19930808@163.com>
2019-02-11 15:39:34 -0800

    ignore the frequency_response since it is just for test

--- source/cal_frequency_response.py
+++ source/cal_frequency_response.py
@@ -14,8 +14,10 @@ analog_b = [1]
 analog_a = [1 / (float(cut_off) / sampling_rate * 2 * pi), 1]
 start_freq = pi / pow(2.0, 12)
 worN = [start_freq * pow(2, i / 10.0) for i in xrange(120)]
-(digital_b, digital_a) = signal.bilinear(analog_b, analog_a)
 (w, h) = signal.freqs(analog_b, analog_a, worN)
+
+# the frequency transformation is not linear and high frequency will be lowered.
+(digital_b, digital_a) = signal.bilinear(analog_b, analog_a)
 (w, h) = signal.freqz(digital_b, digital_a, worN)
 
 fig = pyplot.figure()
