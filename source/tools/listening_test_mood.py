28a2a82ee1487b6a5266f1ecc3cca33d05190a82
hongyu19930808 <hongyu19930808@163.com>
2019-03-23 21:59:00 -0700

    Update listening_test_mood.py

--- source/tools/listening_test_mood.py
+++ source/tools/listening_test_mood.py
@@ -18,10 +18,10 @@ gender = Gender.Male
 age = 25
 
 start_session = 2
-num_session = 2
+num_session = 1
 
 # global variables
-num_excerpt_per_session = 4
+num_excerpt_per_session = 32
 all_excerpts = range(start_session * num_excerpt_per_session, (start_session + num_session) * num_excerpt_per_session)
 records = {excerpt: {} for excerpt in all_excerpts}
 shuffle(all_excerpts)
@@ -36,18 +36,59 @@ shuffle(mood_strs)
 root = tk.Tk()
 frame = tk.Frame(master = root)
 labels = tk.Frame(master = frame)
-question_label_1 = tk.Label(master = labels, font = ('Arial', 20), text = 'Is the mood of the sound')
-question_label_2 = tk.Label(master = labels, font = ('Arial', 20), foreground = 'red', text = mood_strs[0])
-question_label_3 = tk.Label(master = labels, font = ('Arial', 20), text = '?')
+question_label_1 = tk.Label(master = labels, font = ('Arial', 30), text = 'Is the mood of the sound')
+question_label_2 = tk.Label(master = labels, font = ('Arial', 30), foreground = 'red', text = mood_strs[0])
+question_label_3 = tk.Label(master = labels, font = ('Arial', 30), text = '?')
 
 audio = PyAudio()
 stream = audio.open(format = paInt16, channels = 1, rate = 44100, output = True)
+
+def button_no_clicked():
+    if finished == False and button_no['state'] == tk.NORMAL:
+        records[all_excerpts[current_excerpt_index]][mood_strs[current_mood_index]] = 0
+        update_index()
+    
+def button_maybe_clicked():
+    if finished == False and button_maybe['state'] == tk.NORMAL:
+        records[all_excerpts[current_excerpt_index]][mood_strs[current_mood_index]] = 1
+        update_index()
+    
+def button_yes_clicked():
+    if finished == False and button_yes['state'] == tk.NORMAL:
+        records[all_excerpts[current_excerpt_index]][mood_strs[current_mood_index]] = 2
+        update_index()
+        
+def button_repeat_clicked():
+    if finished == False and button_repeat['state'] == tk.NORMAL:
+        global repeat_flag
+        repeat_flag = True
+        
+button_no = tk.Button(master = frame, text = 'Not at all (Press 1)', font = ('Arial', 20),
+                      command = button_no_clicked)
+button_maybe = tk.Button(master = frame, text = 'Somewhat (Press 2)', font = ('Arial', 20),
+                         command = button_maybe_clicked)
+button_yes = tk.Button(master = frame, text = 'Definitely (Press 3)', font = ('Arial', 20),
+                       command = button_yes_clicked)
+button_repeat = tk.Button(master = frame, text = 'Repeat (Press 0)', font = ('Arial', 20),
+                          command = button_repeat_clicked)
+
+def disable_button():
+    button_no['state'] = tk.DISABLED
+    button_maybe['state'] = tk.DISABLED
+    button_yes['state'] = tk.DISABLED
+    button_repeat['state'] = tk.DISABLED
+    
+def enable_button():
+    button_no['state'] = tk.NORMAL
+    button_maybe['state'] = tk.NORMAL
+    button_yes['state'] = tk.NORMAL
+    button_repeat['state'] = tk.NORMAL
 
 def play_excerpt():
     global repeat_flag
     current_playing_index = -1
     start = 0
-    frame_length = 1024
+    frame_length = 16384
     samples = []
     while True:
         if current_playing_index == -1:
@@ -55,10 +96,11 @@ def play_excerpt():
             samples = read_samples()
             if samples is None:
                 break
+            samples *= pow(2, 14)
             start = 0
             end = min(start + frame_length, len(samples))
             frame = samples[start:end]
-            stream.write(raw_audio_string(frame * pow(2, 15)))
+            stream.write(raw_audio_string(frame))
             start += frame_length
             repeat_flag = False
         elif current_playing_index != current_excerpt_index:
@@ -66,11 +108,12 @@ def play_excerpt():
             frame = samples[start:end]
             for i in xrange(len(frame)):
                 frame[i] *= (1.0 - float(i) / len(frame))
-            stream.write(raw_audio_string(frame * pow(2, 15)))
+            stream.write(raw_audio_string(frame))
             start = 0
             samples = read_samples()
             if samples is None:
-                break            
+                break
+            samples *= pow(2, 14)
             current_playing_index = current_excerpt_index
             repeat_flag = False
         else:
@@ -78,9 +121,10 @@ def play_excerpt():
             frame = samples[start:end]
             # check whether it is the last frame of the excerpt
             if start + frame_length >= len(samples):
+                enable_button()
                 for i in xrange(len(frame)):
                     frame[i] *= (1.0 - float(i) / len(frame))
-                stream.write(raw_audio_string(frame * pow(2, 15)))
+                stream.write(raw_audio_string(frame))
                 if repeat_flag == True:
                     start = 0
                     repeat_flag = False
@@ -88,7 +132,7 @@ def play_excerpt():
                     start += frame_length
                     sleep(1)
             else:
-                stream.write(raw_audio_string(frame * pow(2, 15)))
+                stream.write(raw_audio_string(frame))
                 start += frame_length
 
 def read_samples():
@@ -96,6 +140,7 @@ def read_samples():
         return None
     instrument_index = str(all_excerpts[current_excerpt_index]).zfill(3)
     path = './sounds/instrument_' + instrument_index + '.wav'
+    print 'current instrument: ' + str(instrument_index)
     wavefile = wave.open(path, 'r')
     num_frames = wavefile.getnframes()
     frame_rate = wavefile.getframerate()
@@ -113,6 +158,29 @@ def close_audio():
     stream.stop_stream()
     stream.close()
     audio.terminate()
+    
+def output():
+    file_name = email + '-' + gender + '-' + str(age)
+    for i in xrange(num_session):
+        file_name += '-s'
+        file_name += str(start_session + i)
+    fout = open('./moods/' + file_name + '.csv', 'w')
+    all_excerpts.sort()
+    mood_strs = ['Angry', 'Scary', 'Comic', 'Happy', 'Sad', 'Mysterious', 'Romantic', 'Calm']
+    for mood in mood_strs:
+        fout.write(',')
+        fout.write(mood)
+    fout.write('\n')
+    for excerpt in all_excerpts:
+        fout.write('Ins ' + str(excerpt))
+        for mood in mood_strs:
+            fout.write(',')
+            if records[excerpt].has_key(mood):
+                fout.write(str(records[excerpt][mood]))
+            else:
+                fout.write('-1')
+        fout.write('\n')
+    fout.close()    
     
 def center_window(root):
     screen_width = root.winfo_screenwidth()
@@ -132,6 +200,7 @@ def update_index():
         return
     current_mood_index += 1
     if current_mood_index >= len(mood_strs):
+        disable_button()
         current_mood_index = 0
         current_excerpt_index += 1
         shuffle(mood_strs)
@@ -141,26 +210,8 @@ def update_index():
         question_label_1['text'] = 'You have finished the test.'
         question_label_2['text'] = 'Thank you!'
         question_label_3['text'] = ''
-
-def button_no_clicked():
-    if finished == False:
-        records[all_excerpts[current_excerpt_index]][mood_strs[current_mood_index]] = 0
-        update_index()
+        output()
     
-def button_maybe_clicked():
-    if finished == False:
-        records[all_excerpts[current_excerpt_index]][mood_strs[current_mood_index]] = 1
-        update_index()
-    
-def button_yes_clicked():
-    if finished == False:
-        records[all_excerpts[current_excerpt_index]][mood_strs[current_mood_index]] = 2
-        update_index()
-    
-def button_repeat_clicked():
-    global repeat_flag
-    repeat_flag = True
-
 def key_press_event(event):
     if event.char == '1':
         button_no_clicked()
@@ -172,31 +223,22 @@ def key_press_event(event):
         button_repeat_clicked()
 
 def main():
-    
     root.title('Listening Test')
     root.bind(sequence = '<KeyPress>', func = key_press_event)
-    frame.grid(padx = 8, pady = 16)
+    frame.grid(padx = 420, pady = 360)
     labels.grid(row = 0, column = 0, columnspan = 3)
     question_label_1.grid(row = 0, column = 0)
     question_label_2.grid(row = 0, column = 1)
     question_label_3.grid(row = 0, column = 2)
     
-    tk.Label(master = frame, text = ' ', font = ('Arial', 4)).grid(row = 1, column = 0, columnspan = 3)
+    tk.Label(master = frame, text = ' ', font = ('Arial', 12)).grid(row = 1, column = 0, columnspan = 3)
     
-    button_no = tk.Button(master = frame, text = 'Not at all (Press 1)', font = ('Arial', 16), 
-                          command = button_no_clicked)
     button_no.grid(row = 2, column = 0)
-    button_no = tk.Button(master = frame, text = 'Somewhat (Press 2)', font = ('Arial', 16), 
-                          command = button_maybe_clicked)
-    button_no.grid(row = 2, column = 1)
-    button_no = tk.Button(master = frame, text = 'Definitely (Press 3)', font = ('Arial', 16), 
-                          command = button_yes_clicked)
-    button_no.grid(row = 2, column = 2)
-    
-    tk.Label(master = frame, text = ' ', font = ('Arial', 4)).grid(row = 3, column = 0, columnspan = 3)
-    button_repeat = tk.Button(master = frame, text = 'Repeat (Press 0)', font = ('Arial', 16), 
-                          command = button_repeat_clicked)
+    button_maybe.grid(row = 2, column = 1)
+    button_yes.grid(row = 2, column = 2)
+    tk.Label(master = frame, text = ' ', font = ('Arial', 12)).grid(row = 3, column = 0, columnspan = 3)
     button_repeat.grid(row = 4, column = 0, columnspan = 3)
+    disable_button()
     
     start_new_thread(play_excerpt, ())    
     
@@ -205,30 +247,6 @@ def main():
     root.resizable(False, False)
     root.focus()
     root.mainloop()
-    
-    # output
-    file_name = email + '-' + gender + '-' + str(age)
-    for i in xrange(num_session):
-        file_name += '-s'
-        file_name += str(start_session + i)
-    fout = open(file_name + '.csv', 'w')
-    all_excerpts.sort()
-    mood_strs = ['Angry', 'Scary', 'Comic', 'Happy', 'Sad', 'Mysterious', 'Romantic', 'Calm']
-    for mood in mood_strs:
-        fout.write(',')
-        fout.write(mood)
-    fout.write('\n')
-    for excerpt in all_excerpts:
-        fout.write('Ins ' + str(excerpt))
-        for mood in mood_strs:
-            fout.write(',')
-            if records[excerpt].has_key(mood):
-                fout.write(str(records[excerpt][mood]))
-            else:
-                fout.write('-1')
-        fout.write('\n')
-    fout.close()
-    
 
 if __name__ == '__main__':
     main()
